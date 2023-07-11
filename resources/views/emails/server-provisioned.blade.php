4d56c8b23ef601def0a6e754fba630f9687ae92c
Pascal Baljet <pascal@pascalbaljet.nl>
2023-07-11 12:58:36 +0200

    Fix email syntax
    See https://github.com/laravel/framework/issues/47400

--- resources/views/emails/server-provisioned.blade.php
+++ resources/views/emails/server-provisioned.blade.php
@@ -1,31 +1,33 @@
 @component('mail::message')
-    {{ __('Your server \':server\' has been provisioned and is ready to use.', ['server' => $server->name]) }}
 
-    {{ __('You can access your server by clicking the button below.') }}
+{{ __('Your server \':server\' has been provisioned and is ready to use.', ['server' => $server->name]) }}
 
-    @component('mail::button', ['url' => route('servers.show', $server)])
-        {{ __('View Server') }}
-    @endcomponent
+{{ __('You can access your server by clicking the button below.') }}
 
-    {{ __('Instead of accessing your server through the web interface, you can also connect to it using the following command:') }}
+@component('mail::button', ['url' => route('servers.show', $server)])
+{{ __('View Server') }}
+@endcomponent
 
-    @component('mail::panel')
-        `ssh {{ $server->username.'@'.$server->public_ipv4 }}`
-    @endcomponent
+{{ __('Instead of accessing your server through the web interface, you can also connect to it using the following command:') }}
 
-    {{ __('Below you will find the details of your server.') }}
+@component('mail::panel')
+`ssh {{ $server->username.'@'.$server->public_ipv4 }}`
+@endcomponent
 
-    @component('mail::panel')
-        {{ __('Username') }}: `{{ $server->username }}`
-        <br />
-        {{ __('Password') }}: `{{ $server->password }}`
-        <br />
-        {{ __('IP Address') }}: `{{ $server->public_ipv4 }}`
-        <br />
-        {{ __('Database Username') }}: `{{ config('eddy.server_defaults.database_name') }}`
-        <br />
-        {{ __('Database Password') }}: `{{ $server->database_password }}`
-    @endcomponent
+{{ __('Below you will find the details of your server.') }}
 
-    {{ __('Good luck with your new server!') }}
+@component('mail::panel')
+{{ __('Username') }}: `{{ $server->username }}`
+
+{{ __('Password') }}: `{{ $server->password }}`
+
+{{ __('IP Address') }}: `{{ $server->public_ipv4 }}`
+
+{{ __('Database Username') }}: `{{ config('eddy.server_defaults.database_name') }}`
+
+{{ __('Database Password') }}: `{{ $server->database_password }}`
+@endcomponent
+
+{{ __('Good luck with your new server!') }}
+
 @endcomponent
